fad8de61a7862e94932f4e060e9ae35a7a13e90b
huzelin <huzelin1008@163.com>
2019-05-07 16:10:41 +0800

    update

--- cmake/modules/common.cmake
+++ cmake/modules/common.cmake
@@ -2,6 +2,6 @@ file(GLOB_RECURSE common_source "common/*.cc")
 file(GLOB_RECURSE common_test_source "common/*_test.cc")
 if(NOT ${common_test_source})
     list(REMOVE_ITEM common_source ${common_test_source})
-    rtp_add_test(${common_test_source} "common" rtp)
+    rtp_add_test(${common_test_source} "common" rtp_core gtest)
 endif()
 
---
+++ common/thread_pool_test.cc
@@ -0,0 +1,9 @@
+/*!
+ * \file thread_pool_test.cc
+ * \brief The thread pool testing unit
+ */
+#include "gtest/gtest.h"
+
+TEST(TestThreadPool, All) {
+
+}
