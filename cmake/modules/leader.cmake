c5afa72621bc5be6229639c47e6ca810febf6421
huzelin <huzelin1008@163.com>
2019-05-09 13:20:20 +0800

    Add leader

--- cmake/modules/leader.cmake
+++ cmake/modules/leader.cmake
@@ -1,17 +1,17 @@
 file(GLOB_RECURSE leader_source "leader/*.cc")
 
-file(GLOB_RECURSE leader_proto_files "leader/*.proto")
+file(GLOB_RECURSE leader_proto_files "leader/proto/*.proto")
 protobuf_generate_cpp_py(
-    ${PROJECT_SOURCE_DIR}/leader/
+    ${PROJECT_BINARY_DIR}/proto/
     proto_srcs
     proto_hdrs
     proto_python
-    "${PROJECT_SOURCE_DIR}/"
-    "./"
+    "${PROJECT_SOURCE_DIR}/leader"
+    "proto"
     ${leader_proto_files})
 list(APPEND leader_source ${proto_srcs})
 include_directories(
-    ${PROJECT_BINARY_DIR}/leader/
+    ${PROJECT_BINARY_DIR}/proto/
 )
 
 file(GLOB_RECURSE leader_proto_files "leader/proto/*.proto")
