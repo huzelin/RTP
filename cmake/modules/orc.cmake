4303cd02cea549ce05d85058eb673f25c9f062c0
huzelin <huzelin1008@163.com>
2019-05-31 10:21:56 +0800

    update

--- cmake/modules/orc.cmake
+++ cmake/modules/orc.cmake
@@ -1,12 +1,12 @@
-file(GLOB_RECURSE orc_source "workflow/orc/*.cc" "workflow/orc/*.c")
-file(GLOB_RECURSE orc_test_source "workflow/orc/*_test.cc")
+file(GLOB_RECURSE orc_source "orc/*.cc" "orc/*.c")
+file(GLOB_RECURSE orc_test_source "orc/*_test.cc")
 
 list(REMOVE_ITEM orc_source ${orc_test_source})
-list(REMOVE_ITEM orc_source "workflow/orc/workflow/parser/interpret.cc")
+list(REMOVE_ITEM orc_source "orc/workflow/parser/interpret.cc")
 
 foreach(source ${orc_test_source})
     rtp_add_test(${source} "tests" rtp_core gtest)
 endforeach()
 
-add_executable(orc_interpret "workflow/orc/workflow/parser/interpret.cc")
+add_executable(orc_interpret "orc/workflow/parser/interpret.cc")
 target_link_libraries(orc_interpret rtp_core)
